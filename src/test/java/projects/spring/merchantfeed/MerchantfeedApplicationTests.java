af8399c8fea1544e0950ee5016a4315920ce95d7
Angela Sindic <asindic@gmail.com>
2021-03-16 13:35:18 +0100

    initial setup

---
+++ src/test/java/projects/spring/merchantfeed/MerchantfeedApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.spring.merchantfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantfeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
